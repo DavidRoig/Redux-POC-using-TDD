1ab4f9cac254bbd07de1bfcbe184c3666189a76c
David Roig <DRoigHeredia@gmail.com>
2022-11-26 21:01:31 +0100

    refactored file name

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Counter } from "./counter";
 import { Provider } from "react-redux";
 import { useMemo } from "react";
-import { createAppStore } from "./store.js";
+import { createAppStore } from "./store";
 
 export default function App() {
-  const store = useMemo(createAppStore, []);
+  const store = createAppStore();
+  // const store = useMemo(createAppStore, []);
 
   return (
     <Provider store={store}>
--- src/store.tsx
+++ src/store.tsx
@@ -3,5 +3,5 @@ import { counterReducer } from "./counter";
 
 export function createAppStore() {
   const reducer = combineReducers({ counter: counterReducer });
-  return createStore(reducer as any, 0);
+  return createStore(reducer as any, { counter: 0 });
 }
